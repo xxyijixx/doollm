81ca676e155d30f465a3fbc834a81b94d654a1e1
yijixx <xxyijixx@gmail.com>
2024-10-10 18:09:45 +0800

    perf: 更新工作区数据来源

--- server/task.go
+++ server/task.go
@@ -17,12 +17,12 @@ var (
 
 func StartScheduledTask() {
 	scheduledTask := schedule.NewScheduledTask()
-
 	// 每天零点执行 0 0 0 * * ?
 	_, err := scheduledTask.AddTask("0 0,30 * * * ? ", fileService.Traversal)
 	if err != nil {
 		log.Errorf("开启定时任务失败 %v", err)
 	}
+	taskService.Traversal()
 	_, err = scheduledTask.AddTask("0 5,35 * * * ? ", taskService.Traversal)
 	if err != nil {
 		log.Errorf("开启定时任务失败 %v", err)
--- service/workspace/workspace.go
+++ service/workspace/workspace.go
@@ -19,28 +19,41 @@ type WorkspaceService interface {
 	Verify(userid int64) bool
 	Upload(userid int64, documentId int64) error
 	Create(userid int64)
+	SelectByUserId(userid int64) (*model.LlmWorkspace, error)
 }
 
 type WorkspaceServiceImpl struct {
 }
 
 func (w *WorkspaceServiceImpl) Verify(userid int64) bool {
-	mockAuthUser := []int64{1, 6}
-	for _, authUser := range mockAuthUser {
-		if authUser == userid {
-			return true
-		}
+
+	_, err := repo.WorkspacePermission.WithContext(context.Background()).Where(repo.WorkspacePermission.WorkspaceID.IsNotNull(), repo.WorkspacePermission.IsCreate.Is(true)).First()
+	if err != nil {
+		log.Debugf("查询用户工作区错误: %v", err)
+		return false
 	}
-	return false
+	return true
 }
 
 // GetWorkspaceUser 获取工作区用户
 // 返回工作区用户列表和一个key为userid 值为工作区信息的Map
 func (w *WorkspaceServiceImpl) GetWorkspaceUser() ([]*model.LlmWorkspace, map[int64]*model.LlmWorkspace, error) {
 	ctx := context.Background()
-	userWorkspaces, err := repo.LlmWorkspace.WithContext(ctx).Find()
+	userWorkspacePermission, err := repo.WorkspacePermission.WithContext(ctx).Where(repo.WorkspacePermission.IsCreate.Is(true),
+		repo.WorkspacePermission.WorkspaceID.IsNotNull()).Find()
 	if err != nil {
 		return nil, nil, err
+	}
+	// 进行转换
+	userWorkspaces := make([]*model.LlmWorkspace, len(userWorkspacePermission))
+	for i, workspacePermission := range userWorkspacePermission {
+		userWorkspaces[i] = &model.LlmWorkspace{
+			ID:        workspacePermission.ID,
+			Userid:    workspacePermission.UserID,
+			Name:      workspacePermission.WorkspaceID,
+			Slug:      workspacePermission.WorkspaceID,
+			CreatedAt: workspacePermission.CreateTime,
+		}
 	}
 	userWorkspaceMapByUserid := make(map[int64]*model.LlmWorkspace)
 	for _, userWorkspace := range userWorkspaces {
@@ -61,13 +74,14 @@ func (w *WorkspaceServiceImpl) Upload(userid int64, documentId int64) error {
 	if err != nil {
 		return err
 	}
-	workspace, err := repo.LlmWorkspace.WithContext(ctx).Where(repo.LlmWorkspace.Userid.Eq(userid)).First()
+	// 获取用户工作区
+	workspace, err := w.SelectByUserId(userid)
 	if err != nil {
 		return err
 	}
 
 	workspaceDocument, err := repo.LlmWorkspaceDocument.WithContext(ctx).
-		Where(repo.LlmWorkspaceDocument.WorkspaceID.Eq(workspace.ID), repo.LlmWorkspaceDocument.DocumentID.Eq(documentId)).
+		Where(repo.LlmWorkspaceDocument.WorkspaceSlug.Eq(workspace.Slug), repo.LlmWorkspaceDocument.DocumentID.Eq(documentId)).
 		First()
 	if err != nil && err != gorm.ErrRecordNotFound {
 		return err
@@ -108,20 +122,10 @@ func (w *WorkspaceServiceImpl) Upload(userid int64, documentId int64) error {
 	return err
 }
 
-func (w *WorkspaceServiceImpl) Update(userid int64, documentId int64) bool {
-	mockAuthUser := []int64{1, 6}
-	for _, authUser := range mockAuthUser {
-		if authUser == userid {
-			return true
-		}
-	}
-	return false
-}
-
 func (w *WorkspaceServiceImpl) RemoveDocument(userid int64, documentId int64) error {
 	log.Debugf("移除文档UserId=%d, documentId=%d", userid, documentId)
 	ctx := context.Background()
-	workspace, err := repo.LlmWorkspace.WithContext(ctx).Where(repo.LlmWorkspace.Userid.Eq(userid)).First()
+	workspace, err := w.SelectByUserId(userid)
 	if err != nil {
 		return err
 	}
@@ -136,7 +140,7 @@ func (w *WorkspaceServiceImpl) RemoveDocument(userid int64, documentId int64) er
 	// 	return err
 	// }
 	resultInfo, err := repo.LlmWorkspaceDocument.WithContext(ctx).
-		Where(repo.LlmWorkspaceDocument.WorkspaceID.Eq(workspace.ID),
+		Where(repo.LlmWorkspaceDocument.WorkspaceSlug.Eq(workspace.Slug),
 			repo.LlmWorkspaceDocument.DocumentID.Eq(document.ID),
 		).Delete()
 	_ = resultInfo
@@ -150,8 +154,9 @@ func (w *WorkspaceServiceImpl) RemoveDocument(userid int64, documentId int64) er
 	return err
 }
 
+// deprecated 不由该方法进行创建
 func (w *WorkspaceServiceImpl) Create(userid int64) {
-	workspace, err := repo.LlmWorkspace.WithContext(context.Background()).Where(repo.LlmWorkspace.Userid.Eq(userid)).First()
+	workspace, err := w.SelectByUserId(userid)
 	if err != nil {
 		if err != gorm.ErrRecordNotFound {
 			return
@@ -173,4 +178,23 @@ func (w *WorkspaceServiceImpl) Create(userid int64) {
 		Slug:      resp.Workspace.Slug,
 		CreatedAt: time.Now(),
 	})
+}
+
+// SelectByUserId 获取并转换
+func (w *WorkspaceServiceImpl) SelectByUserId(userid int64) (*model.LlmWorkspace, error) {
+	workspacePermission, err := repo.WorkspacePermission.WithContext(context.Background()).Where(repo.WorkspacePermission.WorkspaceID.IsNotNull(),
+		repo.WorkspacePermission.IsCreate.Is(true),
+		repo.WorkspacePermission.UserID.Eq(userid)).First()
+	if err != nil {
+		return nil, err
+	}
+
+	workspace := &model.LlmWorkspace{
+		ID:        workspacePermission.ID,
+		Userid:    workspacePermission.UserID,
+		Name:      workspacePermission.WorkspaceID,
+		Slug:      workspacePermission.WorkspaceID,
+		CreatedAt: workspacePermission.CreateTime,
+	}
+	return workspace, nil
 }
