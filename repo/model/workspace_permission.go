72bdf5c4611d81e8d848c9b88e484fd26769177e
yijixx <xxyijixx@gmail.com>
2024-10-10 11:22:11 +0800

    style: 更新字段约束

--- repo/model/workspace_permission.go
+++ repo/model/workspace_permission.go
@@ -5,10 +5,10 @@ import "time"
 const TableNameWorkspacePermission = "pre_workspace_permissions"
 
 type WorkspacePermission struct {
-	ID          int64     `json:"id"`
+	ID          int64     `gorm:"primaryKey;autoIncrement:true" json:"id"`
 	UserID      int64     `json:"user_id"`
 	IsCreate    bool      `json:"is_create"`
-	WorkspaceID string    `json:"workspace_id"`
+	WorkspaceID string    `gorm:"size:255" json:"workspace_id"`
 	CreateTime  time.Time `json:"create_time"`
 	UpdateTime  time.Time `json:"update_time"`
 }
