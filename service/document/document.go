76d270142cfc9e36edaff96231348244fd3937b4
yijixx <xxyijixx@gmail.com>
2024-10-11 17:04:34 +0800

    feat: 新增清除不在数据库中的文档功能

--- service/document/document.go
+++ service/document/document.go
@@ -6,6 +6,8 @@ import (
 	"doollm/clients/anythingllm/system"
 	"doollm/clients/anythingllm/workspace"
 	"doollm/repo"
+	"doollm/repo/model"
+	"fmt"
 
 	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
@@ -100,4 +102,47 @@ func (d *DocumentServiceImpl) RemoveAll(removeLinkType string) error {
 	repo.LlmDocument.WithContext(ctx).Where(repo.LlmDocument.ID.In(ids...)).Delete()
 	repo.LlmWorkspaceDocument.WithContext(ctx).Where(repo.LlmWorkspaceDocument.DocumentID.In(ids...)).Delete()
 	return nil
+}
+
+// Clear 清除知识库中在数据库中没有保存的文档
+func (d *DocumentServiceImpl) Clear() error {
+	log.Debug("正在清除没有保存的文档信息")
+	ctx := context.Background()
+	documents, err := repo.LlmDocument.WithContext(ctx).Find()
+	if err != nil && err != gorm.ErrRecordNotFound {
+		log.Warnf("查询文档信息错误: %v", err)
+		return err
+	}
+	documentIdMap := make(map[string]*model.LlmDocument)
+	for _, document := range documents {
+		documentIdMap[document.DocID] = document
+	}
+	resp, err := anythingllmClient.QueryDocument()
+	if err != nil {
+		log.Warnf("查询知识库文档信息错误: %v", err)
+		return err
+	}
+	notFoundNum := 0
+	notFoundDocumentLocations := make([]string, 0)
+	for _, folder := range resp.LocalFiles.Items {
+		log.Debugf("当前文件夹: %s\n", folder.Name)
+		for _, document := range folder.Items {
+
+			if _, extst := documentIdMap[document.ID]; !extst {
+				log.Debugf("当前文档ID: %s, 名称：%s, name:[%s] 不存在于数据库\n", document.ID, document.Title, document.Name)
+				notFoundNum++
+				notFoundDocumentLocations = append(notFoundDocumentLocations, fmt.Sprintf("%s/%s", folder.Name, document.Name))
+			}
+		}
+	}
+	log.Debugf("找到%d个文档不存在于数据库", notFoundNum)
+
+	log.Warnf("正在从知识库移除%d个文档...", notFoundNum)
+	// 移除文档
+	_ = notFoundDocumentLocations
+	// err = anythingllmClient.RemoveDocument(system.RemoveDocumentParams{
+	// 	Names: notFoundDocumentLocations,
+	// })
+
+	return err
 }
